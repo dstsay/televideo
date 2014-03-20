d63fb44524bdf79699738c664efa9a6f4ef0cfb3
dstsay <davetsay@gmail.com>
2014-03-20 00:14:31 -0400

    Rails and Boostrap init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Televisit::Application.config.secret_key_base = '92c995e5ba4607320812101e994f2d6579ca8db838e0009688c00064f8b48ffd9a4e91fc8a0ba8ed579a98c6115493f785f5e9770fa2489c4f222e8e7e3c2c6c'
