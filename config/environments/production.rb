5ec962c89739a96cd102357336a93a90d5754220
dstsay <davetsay@gmail.com>
2014-03-31 12:23:44 -0400

    Changing Mail Service to Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,18 +81,17 @@ Televisit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  # Configure for Sendgrid
+  # Configure for MailGun
   config.action_mailer.default_url_options = { :host => 'http://televisit.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'televisit.herokuapp.com',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
   }
+  ActionMailer::Base.delivery_method = :smtp
   
   config.action_mailer.raise_delivery_errors = true
   
