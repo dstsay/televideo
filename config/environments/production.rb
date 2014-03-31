6e0faf21d4970f96be1708ab2b75fc98840391f3
dstsay <davetsay@gmail.com>
2014-03-30 22:40:45 -0400

    SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Televisit::Application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => EBG['SENDGRID_DOMAIN'],
+    :domain         => ENV['SENDGRID_DOMAIN'],
     :enable_starttls_auto => true
   }
   config.action_mailer.raise_delivery_errors = true
