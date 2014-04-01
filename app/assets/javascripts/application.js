e6d485321015823b3a592eb7f610989f6f808865
dstsay <davetsay@gmail.com>
2014-04-01 11:22:43 -0400

    Added Signin page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require turbolinks
 //= require_tree .
 
 // This initiates javascript objects within the page on document ready
